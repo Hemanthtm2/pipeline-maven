12cd5eaf593b6528e6b2d6d9bb99460927ce1b1b
hemanthtm2 <hemanthtm2@gmail.com>
2020-10-20 04:35:19 +0000

    fixes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,6 @@ echo "** Building jar **********"
 
 echo "**************************"
 
- docker run --rm  -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/  -w /app maven:3-alpine "$@"
+WORKSPACE=/root/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm  -v $WORKSPACE:/java-app:/app -v /root/.m2/:/root/.m2/  -w /app maven:3-alpine "$@"
