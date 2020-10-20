9222c50065a25b754b44df41de5dbe41e4150214
hemanthtm2 <hemanthtm2@gmail.com>
2020-10-20 11:20:15 +0000

    test

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,4 +8,4 @@ echo "**************************"
 
 WORKSPACE=/root/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine  
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn test  
